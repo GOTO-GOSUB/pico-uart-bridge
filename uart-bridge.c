fd781061f9e929917e6b9fc587541c63b25d5696
Garry Heather <github@gmhmail.net>
2026-08-11 12:02:20 +0100

    Move UART0 away from WS2812 LED.
    Moves UART0 GP pins away from the WS2812, so that both UARTs are available on the RP2040-Zero development board.

--- uart-bridge.c
+++ uart-bridge.c
@@ -52,8 +52,8 @@ const uart_id_t UART_ID[CFG_TUD_CDC] = {
 		.inst = uart0,
 		.irq = UART0_IRQ,
 		.irq_fn = &uart0_irq_fn,
-		.tx_pin = 16,
-		.rx_pin = 17,
+		.tx_pin = 12,
+		.rx_pin = 13,
 	}, {
 		.inst = uart1,
 		.irq = UART1_IRQ,
